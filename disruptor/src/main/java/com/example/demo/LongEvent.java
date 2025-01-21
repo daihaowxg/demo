d71bb89dba278a1856bfc5ec3352e8eba66ed429
wxg <daihaowxg@gmail.com>
2025-01-21 23:29:51 +0800

    Disruptor 快速入门

---
+++ disruptor/src/main/java/com/example/demo/LongEvent.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+/**
+ * 定义事件
+ */
+public class LongEvent {
+    private long value;
+
+    public void set(long value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "LongEvent{" + "value=" + value + '}';
+    }
+}
---
+++ disruptor/src/main/java/com/example/demo/LongEventMain1.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import com.lmax.disruptor.RingBuffer;
+import com.lmax.disruptor.dsl.Disruptor;
+import com.lmax.disruptor.util.DaemonThreadFactory;
+
+import java.nio.ByteBuffer;
+
+public class LongEventMain1 {
+    public static void main(String[] args) throws InterruptedException {
+        int bufferSize = 1024;
+
+        // 初始化 Disruptor
+        Disruptor<LongEvent> disruptor = new Disruptor<LongEvent>(LongEvent::new, bufferSize, DaemonThreadFactory.INSTANCE);
+        // 设置 Disruptor 的事件处理器
+        disruptor.handleEventsWith(new LongEventHandler());
+        // 启动 Disruptor
+        disruptor.start();
+
+        RingBuffer<LongEvent> ringBuffer = disruptor.getRingBuffer();
+        for (long i = 0; true; i++) {
+            long sequence = ringBuffer.next();
+            try {
+                LongEvent event = ringBuffer.get(sequence);
+                event.set(i);
+            } finally {
+                ringBuffer.publish(sequence);
+            }
+            Thread.sleep(1000);
+        }
+    }
+}
