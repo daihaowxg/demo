c0df9cbd5d6c2a1f6d7ac7a3a71d4340117038a8
wxg <wangxingang@crhms.cn>
2025-03-15 09:46:06 +0800

    feat(lab01_java): 添加 JUC 实验相关代码
    - 新增 lab01_juc模块
    - 添加 ThreadLocal相关示例代码
    - 实现了使用 ThreadLocal 在线程间传递用户信息的功能
    -包含 MyRunnable、ThreadLocalContext 和 ThreadLocalExample 三个类

---
+++ lab01_java/lab01_juc/src/main/java/com/example/threadlocal/ThreadLocalExample.java
@@ -0,0 +1,15 @@
+package com.example.threadlocal;
+
+/**
+ * @author wxg
+ * @since 2025/3/14
+ */
+public class ThreadLocalExample {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            MyRunnable runnable = new MyRunnable("user" + i);
+            new Thread(runnable).start();
+        }
+    }
+}
