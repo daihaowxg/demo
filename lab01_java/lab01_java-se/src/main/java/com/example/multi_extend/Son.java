8a873abd32a3d769b8f83c87f92460d469040b02
wxg <wangxingang@crhms.cn>
2025-03-17 09:39:53 +0800

    refactor: 补充 Son 类中的 show 方法实现
    - 在 Son 类的 show 方法中添加了打印语句，明确表示该方法必须被实现
    - 解决了编译报错问题，确保类的正确编译

--- lab01_java/lab01_java-se/src/main/java/com/example/multi_extend/Son.java
+++ lab01_java/lab01_java-se/src/main/java/com/example/multi_extend/Son.java
@@ -3,6 +3,6 @@ package com.example.multi_extend;
 public class Son implements Father, Mother {
     @Override
     public void show() {
-
+        System.out.println("这里实现类必须实现 show() 方法，否则会编译报错");
     }
 }
