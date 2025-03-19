517e94af045045ff346441c3f5d6dea5795f4311
wxg <wangxingang@crhms.cn>
2025-03-19 10:14:11 +0800

    build:将 Java 版本从 17 降为8
    - 修改了多个模块的 Maven 配置文件，将 Java 编译源和目标版本从 17 改为 8
    - 更新了根 pom.xml 文件，添加 java.version 属性并设置为 8
    - 调整了 MetaspaceOOM 类的运行命令示例
    - 在 DemoApplication 中添加了 ApplicationRunner Bean，用于打印应用启动信息

--- lab01_java/lab01_jvm/src/main/java/com/example/demo/MetaspaceOOM.java
+++ lab01_java/lab01_jvm/src/main/java/com/example/demo/MetaspaceOOM.java
@@ -9,7 +9,11 @@ import javassist.ClassPool;
 public class MetaspaceOOM {
 
     /**
-     * java -XX:MaxMetaspaceSize=10m MetaspaceOOM
+     * java -XX:MaxMetaspaceSize=10m -XX:-UseCompressedClassPointers -XX:+PrintGCDetails -XX:+PrintGCDateStamps com.example.demo.MetaspaceOOM
+     * -XX:MaxMetaspaceSize=10m 限制 Metaspace（元空间）的最大大小为 10MB。
+     * -XX:-UseCompressedClassPointers 禁用压缩类指针，以减少元空间使用量。启用压缩指针时，类的 metadata 可能会被存储在 Compressed Class Space 里，而不是 Metaspace
+     * -XX:+PrintGCDetails 打印 GC 日志 这样可以帮助分析 Metaspace 是否真的导致了 OOM，或者是其他 GC 相关问题。
+     * -XX:+PrintGCDateStamps 打印 GC 时间戳，这样 GC 日志中的时间戳就可以与实际时间进行比较，以确定 GC 是否在 Metaspace 中发生。
      */
     public static void main(String[] args) throws Exception {
         ClassPool classPool = ClassPool.getDefault();
