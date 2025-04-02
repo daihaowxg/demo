993d827da2b13bb7c8692926fc245335a419d7e9
wxg <wangxingang@crhms.cn>
2025-04-02 17:36:39 +0800

    feat(lab01_juc): 添加死锁示例和解决方法
    - 新增 DeadLockDemo 类，演示死锁发生的条件和现象
    - 新增 DeadLockHandle 类，展示如何通过固定顺序获取锁来解决死锁问题

---
+++ lab01_java/lab01_juc/src/main/java/com/example/demo/deadlock/DeadLockDemo.java
@@ -0,0 +1,42 @@
+package com.example.demo.deadlock;
+
+/**
+ * 死锁示例
+ * 1. 互斥条件：互斥资源，即同一时间只能被一个线程使用
+ * 2. 占有并等待：一个线程已经获得资源，在等待其它资源
+ * 3. 不可抢占：线程不能抢占其它线程已经使用的资源
+ * 4. 循环等待：线程1占有资源1，等待资源2，线程2占有资源2，等待资源1，线程1、线程2互为等待，造成死锁
+ * @author wxg
+ * @since 2025/4/2
+ */
+public class DeadLockDemo {
+    public static void main(String[] args) {
+        Object A = new Object();
+        Object B = new Object();
+
+        // 线程1：先锁A，再锁B
+        Runnable task1 = () -> {
+            synchronized (A) {
+                try { Thread.sleep(1000); }
+                catch (InterruptedException e) {}
+                synchronized (B) {
+                    System.out.println("A：do something...");
+                }
+            }
+        };
+
+        // 线程2：先锁B，再锁A
+        Runnable task2 = () -> {
+            synchronized (B) {
+                try { Thread.sleep(1000); }
+                catch (InterruptedException e) {}
+                synchronized (A) {
+                    System.out.println("B：do something...");
+                }
+            }
+        };
+
+        new Thread(task1).start();
+        new Thread(task2).start();
+    }
+}
