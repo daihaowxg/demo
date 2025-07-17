2a4e336b9220fce198930c3347546beff57557ef
wxg <daihaowxg@gmail.com>
2025-07-17 10:37:36 +0800

    feat(mybatis): MyBatis Demo

---
+++ lab11_mybatis/src/main/java/com/example/mapper/AuthorMapper.java
@@ -0,0 +1,23 @@
+package com.example.mapper;
+
+import com.example.model.Author;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface AuthorMapper {
+    /**
+     * 动态更新 Author
+     */
+    int updateAuthorIfNecessary(Author author);
+
+    /**
+     * 用于测试准备数据
+     */
+    void insertAuthor(Author author);
+
+    /**
+     * 用于验证数据
+     */
+    Author findAuthorById(@Param("id") int id);
+}
---
+++ lab11_mybatis/src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Author
+(
+    id       INT PRIMARY KEY,
+    username VARCHAR(255),
+    password VARCHAR(255),
+    email    VARCHAR(255),
+    bio      VARCHAR(255)
+);
